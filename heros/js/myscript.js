41b7d25f132fcd6366ea22edd8c046fa5d277603
Carine <142498633+carinefalchetti@users.noreply.github.com>
2023-10-17 11:37:05 -0300

    Add files via upload

---
+++ heros/js/myscript.js
@@ -0,0 +1,18 @@
+function mudarFrase() {
+    var elemento = gebi("frase")
+    elemento.innerHTML = "Qualquer frase"
+    elemento.style.color = "red";
+    elemento.style.fontSize = "25px";
+    elemento.style.backgroundColor = "yellow";
+    document.body.style.backgroundColor = "blue";
+    elemento.innerHTML = "<img src=\'img/gatos.jpg\' />"
+}
+function trocarTexto(){
+    var elemento = gebi("frase")
+    var nova = gebi("novaFrase").value
+    elemento.innerHTML = nova
+}
+
+function gebi(id){
+    return document.getElementById(id)
+}
